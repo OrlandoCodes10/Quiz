8f3c60abd07ca89d50c0429cb0e649fc1f75f924
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-01-25 01:20:26 -0500

    Creating the quiz code

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question:'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2023?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
+        question: 'Who is the President of US?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b',
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext markup Language',
+        b: 'Cascasing Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a' 
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'None of the above',
+        correct: 'd'
+    }
+]
